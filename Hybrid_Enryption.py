451bc238d963843133dd85cb9bccb4cd3bcfc5f1
Hetvi Patel <hetvi1655@gmail.com>
2025-12-21 21:37:09 +0530

    Hybrid Encryption Implementation.

--- Hybrid_Enryption.py
+++ Hybrid_Enryption.py
@@ -7,7 +7,7 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS7_OAEP
+from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Util.Padding import pad,unpad
 from AES_Engine import aes_encrypt_gcm, aes_decrypt_gcm
 
@@ -17,10 +17,48 @@ def encrypt_content(content_bytes, rsa_public_key):
     aes_key = get_random_bytes(32)
 
     # Encrypt content using AES-GCM
-    cip
+    encrypted_gcm, nonce_gcm, tag_gcm = aes_encrypt_gcm(content_bytes, aes_key)
+
+    # Encrypt AES key using RSA-OAEP
+    rsa_public_key = RSA.import_key(rsa_public_key)
+    cipher_rsa = PKCS1_OAEP.new(rsa_public_key)
+    encrypted_aes_key = cipher_rsa.encrypt(aes_key)
+    return {
+        'encrypted_key' : encrypted_aes_key,
+        'nonce' : nonce_gcm,
+        'tag' : tag_gcm,
+        'ciphertext' : encrypted_gcm
+   }
     
+# Function to decrypt content using RSA-OAEP and AES-256-GCM
+def decrypt_content(encrypted_blob, rsa_private_key):
+    encrypted_aes_key = encrypted_blob['encrypted_key']
+    nonce_gcm = encrypted_blob['nonce']
+    tag_gcm = encrypted_blob['tag']
+    ciphertext = encrypted_blob['ciphertext']
+
+    # Decrypt AES key using RSA-OAEP
+    rsa_private_key = RSA.import_key(rsa_private_key)
+    cipher_rsa = PKCS1_OAEP.new(rsa_private_key)
+    aes_key = cipher_rsa.decrypt(encrypted_aes_key)
+
+    # Decrypt content using AES-GCM
+    decrypt_content = aes_decrypt_gcm(ciphertext, nonce_gcm, aes_key, tag_gcm)
+
+    return decrypt_content
+
 
 data = 'This is a test message for Robust Hybrid Encryption System!'.encode()
-public_key = RSA.import_key(open('public.pem').read())
-private_key = RSA.import_key(open('private.pem').read())
+print("\nOriginal Data:", data)
+# Example usage
+# Generate RSA key pair
+key = RSA.generate(2048)
+private_key = key.export_key()
+public_key = key.publickey().export_key()
+# Encrypt the data
+encrypted_blob = encrypt_content(data, public_key)
+print("\nEncrypted Blob:", encrypted_blob)
+# Decrypt the data
+decrypted_data = decrypt_content(encrypted_blob, private_key)
+print("\nDecrypted Data:", decrypted_data.decode())
 
